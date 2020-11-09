bd92ad66d724661ebd562d20f4b927c5b7a6b298
sheraznabimirza <sheraznabimirza@gmail.com>
2020-11-10 03:42:33 +0500

    removed redundant code, finishing cleanups

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ class QuoteMachine extends React.Component {
         super(props);
         this.state = {
             randomIndex: Math.floor(Math.random() * quoting.length),
-            randomColor: Math.floor(Math.random() * colors.length),
-            activate: ""
+            randomColor: Math.floor(Math.random() * colors.length)
         }
         this.handleClick = this.handleClick.bind(this);
         this.randomize = this.randomize.bind(this);
-        this.morphColor = this.morphColor.bind(this);
     }
 
     randomize() {
@@ -35,13 +33,6 @@ class QuoteMachine extends React.Component {
             });
     }
 
-    morphColor() {
-        this.setState(
-        {
-            activate : "shift-color"
-        });
-    }
-
     handleClick() {
         this.randomize();
     }
@@ -51,15 +42,13 @@ class QuoteMachine extends React.Component {
         }
         const changingBackground = {
             backgroundColor: colors[this.state.randomColor],
-            transition: "all 1s ease",
-            WebkitTransition: "all 1s ease",
-            MozTransition: "all 1s ease"
+            transition: "all 1s ease"
         }
         return (
             <div className="main" style={changingBackground}>
             <div id="quote-box">
                 <h1 id="text" style={changingColor} key={quoting[this.state.randomIndex].quote}> <a><i class="fa fa-quote-left"></i></a> {quoting[this.state.randomIndex].quote}</h1>
-                <h4 id="author" className="text-fade" style={changingColor} key={quoting[this.state.randomIndex].author}>- {quoting[this.state.randomIndex].author}</h4>
+                <h4 id="author" style={changingColor} key={quoting[this.state.randomIndex].author}>- {quoting[this.state.randomIndex].author}</h4>
                 <div className="buttons">
                     <a id="tweet-quote" href="#" title="Tweet this quote!"><i class="fa fa-twitter"></i></a>
                     <Button variant="outline-dark" id="new-quote" onClick={this.handleClick}>New quote</Button>
