e36342d4a6467cc56dddd08f7d531c024486187b
sheraznabimirza <sheraznabimirza@gmail.com>
2020-11-10 03:30:19 +0500

    finalized transitions

--- src/App.js
+++ src/App.js
@@ -50,15 +50,18 @@ class QuoteMachine extends React.Component {
             color: colors[this.state.randomColor]
         }
         const changingBackground = {
-            backgroundColor: colors[this.state.randomColor]
+            backgroundColor: colors[this.state.randomColor],
+            transition: "all 1s ease",
+            WebkitTransition: "all 1s ease",
+            MozTransition: "all 1s ease"
         }
         return (
             <div className="main" style={changingBackground}>
             <div id="quote-box">
-                <h1 id="text" className="text-fade" style={changingColor} key={quoting[this.state.randomIndex].quote}> "{quoting[this.state.randomIndex].quote}</h1>
+                <h1 id="text" style={changingColor} key={quoting[this.state.randomIndex].quote}> <a><i class="fa fa-quote-left"></i></a> {quoting[this.state.randomIndex].quote}</h1>
                 <h4 id="author" className="text-fade" style={changingColor} key={quoting[this.state.randomIndex].author}>- {quoting[this.state.randomIndex].author}</h4>
                 <div className="buttons">
-                    <a id="tweet-quote" href="#"><i class="fa fa-twitter"></i></a>
+                    <a id="tweet-quote" href="#" title="Tweet this quote!"><i class="fa fa-twitter"></i></a>
                     <Button variant="outline-dark" id="new-quote" onClick={this.handleClick}>New quote</Button>
                 </div>
                 
